71797a719156519da840c4d611a76d88a9eb0d0f
Bilguun Banzragch <LP230113@G301-CC0174.local>
2023-09-27 17:15:29 +0800

    blog section heseg hiiv

--- lesson_02_use_effect/pages/index.js
+++ lesson_02_use_effect/pages/index.js
@@ -1,34 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const index = () => {
-  const [count, setCount] = useState(0);
-  const plus = () => {
-    setCount(count + 1);
+const Home = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchData = async () => {
+    const res = await fetch("https://dev.to/api/articles?per_page=9");
+    const data = await res.json();
+    console.log(data);
+    setBlogs(data);
   };
-  const minus = () => {
-    setCount(count - 1);
-  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div className="h-screen bg-slate-400 flex flex-col justify-center items-center">
-      <h1 className="mb-6">
-        Too:<span>{count}</span>
-      </h1>
-      <div>
-        <button
-          className="border-[3px] mx-4 border-green-800  px-4"
-          onClick={plus}
-        >
-          Plus
-        </button>
-        <button
-          className="border-[3px] mx-4 border-red-800 px-4"
-          onClick={minus}
-        >
-          Minus
-        </button>
-      </div>
+    <div>
+      <section>
+        <h2>All Blog Post</h2>
+        <div className=" mx-80 grid grid-cols-3 gap-3">
+          {blogs.map((blog) => {
+            return (
+              <div className="border flex flex-col rounded-[10px] justify-center">
+                <img
+                  className="rounded-[10px] m-4"
+                  src={blog.cover_image ? blog.cover_image : "/ws.jpeg"}
+                />
+                <div className="m-4">
+                  <span className=" bg-slate-100 rounded-full text-blue-800 p-1">
+                    {blog?.type_of}
+                  </span>
+                  <h2 className="font-bold text-[25px] my-4">{blog.title}</h2>
+                  <div className="flex">
+                    <img
+                      className="w-[40px] h-[40px] rounded-full"
+                      src={blog.user.profile_image}
+                    />
+                    <p className="mx-10 text-gray-500">{blog.user.name}</p>
+                    <span className="text-gray-400">
+                      {blog.readable_publish_date}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </div>
   );
 };
 
-export default index;
+export default Home;

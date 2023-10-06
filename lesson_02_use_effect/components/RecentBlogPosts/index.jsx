6561a75514599ed331d55f3571fad2fb54209ccb
Bilguun Banzragch <LP230113@G301-CC0174.local>
2023-10-06 18:22:41 +0800

    last

---
+++ lesson_02_use_effect/components/RecentBlogPosts/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const RecentBlogPosts = () => {
+  return (
+    <div>
+      <h1 className="font-semibold text-2xl">Recent Blog Posts</h1>
+      <div className="flex bg-red-200">
+        <div className="flex-1 bg-slate-200">hahahah</div>
+        <div className="flex-1 bg-green-300">
+          <div className="bg-yellow-200 flex-1">hahhaha</div>
+          <div className="bg-blue-200 flex-1">ahahahahha</div>
+        </div>
+      </div>
+      <div className="bg-red-200">hihiih</div>
+    </div>
+  );
+};
+
+export default RecentBlogPosts;
